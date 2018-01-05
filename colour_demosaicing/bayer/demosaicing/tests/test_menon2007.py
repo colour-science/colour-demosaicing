83b66128a4f9f198ab57d57304508064694f4742
Thomas Mansencal <thomas.mansencal@gmail.com>
2018-01-05 13:56:51 +1300

    Update copyright statements.

--- colour_demosaicing/bayer/demosaicing/tests/test_menon2007.py
+++ colour_demosaicing/bayer/demosaicing/tests/test_menon2007.py
@@ -17,7 +17,7 @@ from colour_demosaicing import TESTS_RESOURCES_DIRECTORY
 from colour_demosaicing.bayer import demosaicing_CFA_Bayer_Menon2007
 
 __author__ = 'Colour Developers'
-__copyright__ = 'Copyright (C) 2015-2017 - Colour Developers'
+__copyright__ = 'Copyright (C) 2015-2018 - Colour Developers'
 __license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-science@googlegroups.com'
