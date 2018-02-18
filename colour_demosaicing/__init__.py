34456ff989745921d0f130fa01f8d2afd038caf8
Thomas Mansencal <thomas.mansencal@gmail.com>
2018-02-19 00:16:28 +1300

    Raise package version to 0.1.3.

--- colour_demosaicing/__init__.py
+++ colour_demosaicing/__init__.py
@@ -42,7 +42,7 @@ __application_name__ = 'Colour - Demosaicing'
 
 __major_version__ = '0'
 __minor_version__ = '1'
-__change_version__ = '2'
+__change_version__ = '3'
 __version__ = '.'.join(
     (__major_version__,
      __minor_version__,
