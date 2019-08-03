ecb0dd2a4a25d11391de677ac955b742acc365c5
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-08-03 15:59:21 +0200

    Update "export_todo.py" utility module.

--- utilities/export_todo.py
+++ utilities/export_todo.py
@@ -35,7 +35,7 @@ About
 | Copyright © 2015-2019 – Colour Developers – `colour-science@googlegroups.com <colour-science@googlegroups.com>`_
 | This software is released under terms of New BSD License: https://opensource.org/licenses/BSD-3-Clause
 | `https://github.com/colour-science/colour-demosaicing <https://github.com/colour-science/colour-demosaicing>`_
-""" [1:]
+"""[1:]
 
 
 def extract_todo_items(root_directory):
