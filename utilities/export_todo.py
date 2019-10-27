e85a7eefd22e68dd31f8719f97ff58f6e877a822
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-10-27 21:31:06 +1300

    Update "export_todo.py" utility.

--- utilities/export_todo.py
+++ utilities/export_todo.py
@@ -33,11 +33,11 @@ About
 
 | **Colour - Demosaicing** by Colour Developers
 | Copyright © 2015-2019 – Colour Developers – \
-`colour-science@googlegroups.com <colour-science@googlegroups.com>`_
+`colour-science@googlegroups.com <colour-science@googlegroups.com>`__
 | This software is released under terms of New BSD License: \
 https://opensource.org/licenses/BSD-3-Clause
 | `https://github.com/colour-science/colour-demosaicing \
-<https://github.com/colour-science/colour-demosaicing>`_
+<https://github.com/colour-science/colour-demosaicing>`__
 """ [1:]
 
 
