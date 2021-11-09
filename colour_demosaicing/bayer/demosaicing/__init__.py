c5d30de5ce4550c8e8f0ef18f651cd3433b0c2c1
Thomas Mansencal <thomas.mansencal@gmail.com>
2021-11-09 22:45:16 +1300

    Format imports.

--- colour_demosaicing/bayer/demosaicing/__init__.py
+++ colour_demosaicing/bayer/demosaicing/__init__.py
@@ -2,8 +2,10 @@
 
 from .bilinear import demosaicing_CFA_Bayer_bilinear
 from .malvar2004 import demosaicing_CFA_Bayer_Malvar2004
-from .menon2007 import (demosaicing_CFA_Bayer_DDFAPD,
-                        demosaicing_CFA_Bayer_Menon2007)
+from .menon2007 import (
+    demosaicing_CFA_Bayer_DDFAPD,
+    demosaicing_CFA_Bayer_Menon2007,
+)
 
 __all__ = []
 __all__ += [
