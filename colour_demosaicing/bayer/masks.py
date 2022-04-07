e4df06d582e572495ab36c68f6986dd369c36251
hahnec <christopher.hahne@unibe.ch>
2022-04-07 20:12:17 +0200

    fix(type): add loop again to comply with return type

--- colour_demosaicing/bayer/masks.py
+++ colour_demosaicing/bayer/masks.py
@@ -79,4 +79,4 @@ def masks_CFA_Bayer(
     for channel, (y, x) in zip(pattern, [(0, 0), (0, 1), (1, 0), (1, 1)]):
         channels[channel][y::2, x::2] = 1
 
-    return tuple(channels)
+    return tuple(channel for channel in channels)
