08d719c29b2a212cb9d59955976f22fa6a5e65da
Thomas Mansencal <thomas.mansencal@gmail.com>
2023-08-27 12:12:24 +1200

    Fix various static typing issues.

--- colour_demosaicing/bayer/demosaicing/menon2007.py
+++ colour_demosaicing/bayer/demosaicing/menon2007.py
@@ -140,12 +140,7 @@ examples_merge_from_raw_files_with_post_demosaicing.ipynb>`__.
     C_V = np.where(B_m == 1, B - G_V, C_V)
 
     D_H = np.abs(C_H - np.pad(C_H, ((0, 0), (0, 2)), mode="reflect")[:, 2:])
-    D_V = np.abs(
-        C_V
-        - np.pad(C_V, ((0, 2), (0, 0)), mode="reflect")[  # pyright: ignore
-            2:, :
-        ]
-    )
+    D_V = np.abs(C_V - np.pad(C_V, ((0, 2), (0, 0)), mode="reflect")[2:, :])
 
     del h_0, h_1, CFA, C_V, C_H
 
