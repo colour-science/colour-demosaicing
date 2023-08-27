de5fbd4be3c97457e392f63b229d4629440451f0
Thomas Mansencal <thomas.mansencal@gmail.com>
2023-08-27 12:31:42 +1200

    Update "conf.py" file.

--- docs/conf.py
+++ docs/conf.py
@@ -4,8 +4,12 @@ Colour - Demosaicing - Documentation Configuration
 """
 
 import re
+import sys
+from pathlib import Path
 
-import colour_demosaicing as package
+sys.path.append(str(Path(__file__).parent.parent))
+
+import colour_demosaicing as package  # noqa: E402
 
 basename = re.sub(
     "_(\\w)", lambda x: x.group(1).upper(), package.__name__.title()
