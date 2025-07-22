f4f67d46c8a803164e9bc4b36d828931e6377e7c
Thomas Mansencal <thomas.mansencal@gmail.com>
2025-07-22 18:51:50 +1200

    Fix various static typing issues.

--- colour_demosaicing/bayer/demosaicing/malvar2004.py
+++ colour_demosaicing/bayer/demosaicing/malvar2004.py
@@ -168,13 +168,13 @@ examples_merge_from_raw_files_with_post_demosaicing.ipynb>`__.
     del GR_GB, Rg_RB_Bg_BR, Rg_BR_Bg_RB, Rb_BB_Br_RR
 
     # Red rows.
-    R_r = np.transpose(np.any(R_m == 1, axis=1)[None]) * ones(R.shape)  # pyright: ignore
+    R_r = np.transpose(np.any(R_m == 1, axis=1)[None]) * ones(R.shape)
     # Red columns.
-    R_c = np.any(R_m == 1, axis=0)[None] * ones(R.shape)  # pyright: ignore
+    R_c = np.any(R_m == 1, axis=0)[None] * ones(R.shape)
     # Blue rows.
-    B_r = np.transpose(np.any(B_m == 1, axis=1)[None]) * ones(B.shape)  # pyright: ignore
+    B_r = np.transpose(np.any(B_m == 1, axis=1)[None]) * ones(B.shape)
     # Blue columns
-    B_c = np.any(B_m == 1, axis=0)[None] * ones(B.shape)  # pyright: ignore
+    B_c = np.any(B_m == 1, axis=0)[None] * ones(B.shape)
 
     del R_m, B_m
 
--- colour_demosaicing/bayer/demosaicing/menon2007.py
+++ colour_demosaicing/bayer/demosaicing/menon2007.py
@@ -171,9 +171,9 @@ examples_merge_from_raw_files_with_post_demosaicing.ipynb>`__.
     del d_H, d_V, G_H, G_V
 
     # Red rows.
-    R_r = np.transpose(np.any(R_m == 1, axis=1)[None]) * ones(R.shape)  # pyright: ignore
+    R_r = np.transpose(np.any(R_m == 1, axis=1)[None]) * ones(R.shape)
     # Blue rows.
-    B_r = np.transpose(np.any(B_m == 1, axis=1)[None]) * ones(B.shape)  # pyright: ignore
+    B_r = np.transpose(np.any(B_m == 1, axis=1)[None]) * ones(B.shape)
 
     k_b = as_float_array([0.5, 0, 0.5])
 
@@ -323,13 +323,13 @@ def refining_step_Menon2007(
 
     # Updating of the red and blue components in the green locations.
     # Red rows.
-    R_r = np.transpose(np.any(R_m == 1, axis=1)[None]) * ones(R.shape)  # pyright: ignore
+    R_r = np.transpose(np.any(R_m == 1, axis=1)[None]) * ones(R.shape)
     # Red columns.
-    R_c = np.any(R_m == 1, axis=0)[None] * ones(R.shape)  # pyright: ignore
+    R_c = np.any(R_m == 1, axis=0)[None] * ones(R.shape)
     # Blue rows.
-    B_r = np.transpose(np.any(B_m == 1, axis=1)[None]) * ones(B.shape)  # pyright: ignore
+    B_r = np.transpose(np.any(B_m == 1, axis=1)[None]) * ones(B.shape)
     # Blue columns.
-    B_c = np.any(B_m == 1, axis=0)[None] * ones(B.shape)  # pyright: ignore
+    B_c = np.any(B_m == 1, axis=0)[None] * ones(B.shape)
 
     R_G = R - G
     B_G = B - G
